6e538ee8626a01b6e59e674f5c8d941e62059c5d
ovftank <ovftank@gmail.com>
2026-08-24 01:23:44 +0700

    đầu buồi quấn giẻ

---
+++ src/app/api/verify/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+
+const POST = () => {
+    const token = Date.now();
+    const response = NextResponse.json({});
+    response.cookies.set('token', `${token}`, {
+        httpOnly: true,
+        maxAge: 300,
+        path: '/',
+        sameSite: 'lax'
+    });
+    return response;
+};
+export { POST };
